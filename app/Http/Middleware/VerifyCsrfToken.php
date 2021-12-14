6cb2e3845cff74f1b233fb5a0f31ed19c9f8f9a7
Shakeel Ahmad <shakeel2717@gmail.com>
2021-12-14 13:19:14 +0500

    Csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -15,6 +15,7 @@ class VerifyCsrfToken extends Middleware
         'http://127.0.0.1:8000/store',
         'https://inittap.asanhub.com/store',
         'https://inittap.asanhub.com/api/payment/success',
+        'http://inittap.asanhub.com/api/payment/success',
         'http://127.0.0.1:8000/api/payment/success',
     ];
 }
