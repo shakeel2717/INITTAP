6e1883099dc059bcec3465246604239bbfa58841
Shakeel Ahmad <shakeel2717@gmail.com>
2022-01-01 01:01:22 +0500

    Landing Page Footer Designed

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -538,7 +538,10 @@
                                     <div class="sent-message">{{ session('success') }}</div>
                                 </div>
                             @endif
-                            <div class="text-center"><button type="submit">Send Message</button></div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary btn-block my-3">Send Message</button>
+
+                            </div>
                         </form>
                     </div>
 
@@ -552,66 +555,16 @@
     <!-- ======= Footer ======= -->
     <footer id="footer">
 
-        <div class="footer-top">
+        <div class="footer-top border">
             <div class="container">
                 <div class="row">
-
-                    <div class="col-lg-3 col-md-6 footer-contact">
-                        <h3>Flexor</h3>
-                        <p>
-                            A108 Adam Street <br>
-                            New York, NY 535022<br>
-                            United States <br><br>
-                            <strong>Phone:</strong> +1 5589 55488 55<br>
-                            <strong>Email:</strong> info@example.com<br>
+                    <div class="col-12 footer-contact text-center">
+                        <a href="index.html"><img src="{{ asset('assets/img/brand/logo-dark.svg') }}"
+                                alt="{{ env('APP_NAME') }}" width="150" class="img-fluid"></a>
+                        <p class="footer-text mt-4">{{ env('APP_NAME') }} © {{ date('Y') }} All right reserved
                         </p>
                     </div>
-
-                    <div class="col-lg-2 col-md-6 footer-links">
-                        <h4>Useful Links</h4>
-                        <ul>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="col-lg-3 col-md-6 footer-links">
-                        <h4>Our Services</h4>
-                        <ul>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-                            <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 footer-newsletter">
-                        <h4>Join Our Newsletter</h4>
-                        <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-                        <form action="" method="post">
-                            <input type="email" name="email"><input type="submit" value="Subscribe">
-                        </form>
-                    </div>
-
                 </div>
-            </div>
-        </div>
-
-        <div class="container d-lg-flex py-4">
-
-            <div class="me-lg-auto text-center text-lg-start">
-
-            </div>
-            <div class="social-links text-center text-lg-right pt-3 pt-lg-0">
-                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
             </div>
         </div>
     </footer><!-- End Footer -->
