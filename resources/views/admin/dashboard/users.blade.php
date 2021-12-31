1b63fcef39bfa90b150a8f1e266e4091f1354b9b
Shakeel Ahmad <shakeel2717@gmail.com>
2022-01-01 01:28:14 +0500

    Admin now can see user public profile link

--- resources/views/admin/dashboard/users.blade.php
+++ resources/views/admin/dashboard/users.blade.php
@@ -18,8 +18,10 @@
                                 <th scope="col">Password</th>
                                 <th scope="col">Status</th>
                                 <th scope="col">Join Date</th>
+                                <th scope="col">Profle Link</th>
                                 <th scope="col">Shipping Address</th>
                                 <th scope="col">Edit</th>
+                                <th scope="col">View Profile</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -30,10 +32,31 @@
                                     <td>Password Protected</td>
                                     <td>{{ $user->status }}</td>
                                     <td>{{ $user->created_at }}</td>
+                                    <td>
+                                        <div class="input-group input-group-merge">
+                                            <input type="text" id="iconExample" class="form-control"
+                                                value="{{ route('user.public.profile', ['username' => $user->username]) }}">
+
+                                            <a class="js-clipboard input-group-append" href="javascript:;"
+                                                data-hs-clipboard-options='{
+                                                        "contentTarget": "#iconExample",
+                                                        "classChangeTarget": "#iconExampleLinkIcon",
+                                                        "defaultClass": "tio-copy",
+                                                        "successClass": "tio-done"
+                                                    }'>
+                                                <span class="input-group-text">
+                                                    <span id="iconExampleLinkIcon" class="tio-copy"></span>
+                                                </span>
+                                            </a>
+                                        </div>
+                                    </td>
                                     <td><a href="{{ route('admin.dashboard.userShow', ['id' => $user->id]) }}"
                                             class="btn btn-sm btn-primary">View Address</a></td>
                                     <td><a href="{{ route('admin.dashboard.userEdit', ['id' => $user->id]) }}"
                                             class="btn btn-sm btn-primary">Edit User</a></td>
+                                    <td><a target="_blank"
+                                            href="{{ route('user.public.profile', ['username' => $user->username]) }}"
+                                            class="btn btn-sm btn-primary">View Profile</a></td>
                                 </tr>
                             @empty
                                 <tr>
