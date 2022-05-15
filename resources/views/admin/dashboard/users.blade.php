6b6598caf0b4e88e22dc0fcdbc5669fc523e06db
Zakaria Barakat <zakibarakaat@gmail.com>
2022-05-15 09:25:56 +0300

    Added mobile number on users dashboard

--- resources/views/admin/dashboard/users.blade.php
+++ resources/views/admin/dashboard/users.blade.php
@@ -15,6 +15,7 @@
                             <tr>
                                 <th scope="col">Full Name</th>
                                 <th scope="col">Email</th>
+                                <th scope="col">Mobile Number</th>
                                 <th scope="col">Password</th>
                                 <th scope="col">Status</th>
                                 <th scope="col">Join Date</th>
@@ -31,6 +32,7 @@
                                 <tr>
                                     <td>{{ $user->name }}</td>
                                     <td>{{ $user->email }}</td>
+                                    <td>{{ $user->mobile }}</td>
                                     <td>Password Protected</td>
                                     <td>{{ $user->status }}</td>
                                     <td>{{ $user->created_at }}</td>
